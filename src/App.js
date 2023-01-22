bfc5091825ebd32b487206cd32e3a64b14060739
Afzal Nomani <afzalnomani1@gmail.com>
2023-01-22 18:29:32 +0530

    connected json server database

--- src/App.js
+++ src/App.js
@@ -3,22 +3,57 @@ import BookList from "./Components/BookList";
 import Header from "./Components/Header";
 import NewBook from "./Components/NewBook";
 import "./App.css";
+import axios from "axios";
+import { useEffect } from "react";
 
 export default function App() {
-  const [book, setBook] = useState([]);
-  const [editBook, setEditBook] = useState();
-  const [deleteBook, setDeleteBook] = useState();
+  const [books, setBooks] = useState([]);
 
-  const newBookHandler = function (data) {
-    console.log(data);
-    setBook((prevData) => [data, ...prevData]);
+  const newBookHandler = async function (data) {
+    const response = await axios.post("http://localhost:3001/books", {
+      title: data.title,
+    });
+
+    setBooks((prevData) => [response.data, ...prevData]);
   };
+
+  const fetchBook = async function () {
+    const response = await axios.get("http://localhost:3001/books");
+
+    setBooks(response.data);
+  };
+
+  useEffect(() => {
+    fetchBook();
+  }, []);
+
+  const editBookHandler = (newtitle, id) => {
+    const updatedBook = books.map((book) => {
+      if (id === book.id) return { ...book, title: newtitle };
+
+      return book;
+    });
+
+    setBooks(updatedBook);
+  };
+
+  const deleteBookHandler = (id) => {
+    const updatedBook = books.filter((book) => {
+      return id !== book.id;
+    });
+    setBooks(updatedBook);
+  };
+
   return (
     <div>
       <Header></Header>
       <div className="main">
         <NewBook onSubmit={newBookHandler} />
-        <BookList books={book} />
+        <BookList
+          books={books}
+          onDelete={deleteBookHandler}
+          onEditSave={editBookHandler}
+        />
       </div>
     </div>
   );
